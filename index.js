a79be01dabf8840700ccf359fe7013275e8e2b32
volysandro <volysandro@users.noreply.github.com>
2018-11-07 14:37:07 +0100

    db

---
+++ index.js
@@ -0,0 +1,21 @@
+(function(){
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBXxghSb0Rou0UJDjVdTcWt5IO_uPDmhZI",
+    authDomain: "buyre-dbd1b.firebaseapp.com",
+    databaseURL: "https://buyre-dbd1b.firebaseio.com",
+    projectId: "buyre-dbd1b",
+    storageBucket: "buyre-dbd1b.appspot.com",
+    messagingSenderId: "251732579641"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+  
+});
